9e4d9d1ac35e83cacf3a6d5ad839db0422219d80
Sean <swg4941@wmich.edu>
2020-06-27 19:11:44 -0400

    Initial Commit

---
+++ AuctionReports/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuctionReports
+//
+//  Created by Sean Phillip Thompson on 6/27/20.
+//  Copyright © 2020 Sean Phillip Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
