3fe6713c5a5aee0e9fddfdc4ceee26b828ca14e4
Sean <swg4941@wmich.edu>
2020-07-15 18:45:49 -0400

    updated UI design. sendtoDB is working now

--- AuctionReports/InspectionViewController.swift
+++ AuctionReports/InspectionViewController.swift
@@ -23,14 +23,18 @@ class InspectionViewController : UIViewController{
     
     var carsTable: ViewController?
     
+    
+    
     @IBOutlet weak var commentsAlert: UIButton!
     
+    //This is the popup that shows when you click the label button
     @IBAction func commentsAlert(_ sender: UIButton) {
         
         alertController = UIAlertController(title: "Comments", message: "Enter any comments below", preferredStyle: .alert)
         
+        //Any page with no comments is left unspecified
         alertController?.addTextField(
-            configurationHandler: {(textField: UITextField!) in textField.placeholder = "None"})
+            configurationHandler: {(textField: UITextField!) in textField.placeholder = "Unspecified"})
         
         let action = UIAlertAction(title: "Submit",
                                    style: UIAlertAction.Style.default,
@@ -46,52 +50,107 @@ class InspectionViewController : UIViewController{
         
     }
     
-    @IBOutlet weak var greenHexButton: UIButton!
+    @IBOutlet weak var rating1: UIButton!
     
-    @IBAction func greenHexButton(_ sender: UIButton) {
-        
-         sender.isSelected = !sender.isSelected
-        
-        if(yellowHexButton.isSelected){
-            yellowHexButton.isSelected = !yellowHexButton.isSelected
-        }
-        
-        if(redHexButton.isSelected){
-            redHexButton.isSelected = !redHexButton.isSelected
-        }
-        
-    }
-    
-   
-    @IBOutlet weak var yellowHexButton: UIButton!
-    
-    @IBAction func yellowHexButton(_ sender: UIButton) {
+    @IBAction func rating1(_ sender: UIButton) {
         
         sender.isSelected = !sender.isSelected
         
-        if(greenHexButton.isSelected){
-            greenHexButton.isSelected = !greenHexButton.isSelected
+        if(rating2.isSelected){
+            rating2.isSelected = !rating2.isSelected
         }
-        
-        if(redHexButton.isSelected){
-            redHexButton.isSelected = !redHexButton.isSelected
+        if(rating3.isSelected){
+            rating3.isSelected = !rating3.isSelected
+        }
+        if(rating4.isSelected){
+            rating4.isSelected = !rating4.isSelected
+        }
+        if(rating5.isSelected){
+            rating5.isSelected = !rating5.isSelected
         }
     }
     
-    @IBOutlet weak var redHexButton: UIButton!
+    @IBOutlet weak var rating2: UIButton!
     
-    @IBAction func redHexButton(_ sender: UIButton) {
+    @IBAction func rating2(_ sender: UIButton) {
         
         sender.isSelected = !sender.isSelected
         
-        if(greenHexButton.isSelected){
-            greenHexButton.isSelected = !greenHexButton.isSelected
+        if(rating1.isSelected){
+            rating1.isSelected = !rating1.isSelected
         }
-        
-        if(yellowHexButton.isSelected){
-            yellowHexButton.isSelected = !yellowHexButton.isSelected
-        }    }
+        if(rating3.isSelected){
+            rating3.isSelected = !rating3.isSelected
+        }
+        if(rating4.isSelected){
+            rating4.isSelected = !rating4.isSelected
+        }
+        if(rating5.isSelected){
+            rating5.isSelected = !rating5.isSelected
+        }
+    }
     
+    
+    @IBOutlet weak var rating3: UIButton!
+    
+    @IBAction func rating3(_ sender: UIButton) {
+        
+        sender.isSelected = !sender.isSelected
+        
+        if(rating2.isSelected){
+            rating2.isSelected = !rating2.isSelected
+        }
+        if(rating1.isSelected){
+            rating3.isSelected = !rating3.isSelected
+        }
+        if(rating4.isSelected){
+            rating4.isSelected = !rating4.isSelected
+        }
+        if(rating5.isSelected){
+            rating5.isSelected = !rating5.isSelected
+        }
+    }
+    
+    
+    @IBOutlet weak var rating4: UIButton!
+    
+    @IBAction func rating4(_ sender: UIButton) {
+        
+        sender.isSelected = !sender.isSelected
+        
+        if(rating2.isSelected){
+            rating2.isSelected = !rating2.isSelected
+        }
+        if(rating3.isSelected){
+            rating3.isSelected = !rating3.isSelected
+        }
+        if(rating1.isSelected){
+            rating4.isSelected = !rating4.isSelected
+        }
+        if(rating5.isSelected){
+            rating5.isSelected = !rating5.isSelected
+        }
+    }
+    
+    @IBOutlet weak var rating5: UIButton!
+    
+    @IBAction func rating5(_ sender: UIButton) {
+        
+        sender.isSelected = !sender.isSelected
+        
+        if(rating2.isSelected){
+            rating2.isSelected = !rating2.isSelected
+        }
+        if(rating3.isSelected){
+            rating3.isSelected = !rating3.isSelected
+        }
+        if(rating4.isSelected){
+            rating4.isSelected = !rating4.isSelected
+        }
+        if(rating1.isSelected){
+            rating5.isSelected = !rating5.isSelected
+        }
+    }
     
     //Outlet and action function for the continue button. Used in every modal except the last
     @IBOutlet weak var continueButton: UIButton!
@@ -99,22 +158,42 @@ class InspectionViewController : UIViewController{
         
     //Check which button was selected before continuing to the next page
         print(self.commentsList![page]! as Any)
-        if(greenHexButton.isSelected){
+        
+        if(rating1.isSelected){
             _ = checkList!.updateValue(1, forKey: page)
-            print(self.checkList![page]! as Any)
             page += 1
-        }else if(yellowHexButton.isSelected){
+        }else if(rating2.isSelected){
             _ = checkList!.updateValue(2, forKey: page)
-            print(self.checkList![page]! as Any)
-             page += 1
-        }else if(redHexButton.isSelected){
+            page += 1
+        }else if(rating3.isSelected){
             _ = checkList!.updateValue(3, forKey: page)
-            print(self.checkList![page]! as Any)
-             page += 1
-            
+            page += 1
+        }else if(rating4.isSelected){
+            _ = checkList!.updateValue(4, forKey: page)
+            page += 1
+        }else if(rating5.isSelected){
+            _ = checkList!.updateValue(5, forKey: page)
+            page += 1
         }else{
             return
         }
+        
+//        if(greenHexButton.isSelected){
+//            _ = checkList!.updateValue(1, forKey: page)
+//            print(self.checkList![page]! as Any)
+//            page += 1
+//        }else if(yellowHexButton.isSelected){
+//            _ = checkList!.updateValue(2, forKey: page)
+//            print(self.checkList![page]! as Any)
+//             page += 1
+//        }else if(redHexButton.isSelected){
+//            _ = checkList!.updateValue(3, forKey: page)
+//            print(self.checkList![page]! as Any)
+//             page += 1
+//
+//        }else{
+//            return
+//        }
         
         //create identifier for the next segue by inserting the page number
          var identifier = "InspectionSegue"
@@ -123,7 +202,7 @@ class InspectionViewController : UIViewController{
              print(identifier)
          
          //If it is not the last inspection modal, segue to the next.
-        if(identifier != "Inspection15Segue"){
+        if(identifier != "Inspection12Segue"){
          
             self.performSegue(withIdentifier:identifier, sender: self)
         }else{
@@ -132,7 +211,9 @@ class InspectionViewController : UIViewController{
             let sendData = sendtoDB()
             sendData.exportData(selectedCar: self.selectedCar, checkList:
             self.checkList!,commentsList: self.commentsList!)
-            //TODO add code for removing table row cell        }
+            
+            self.performSegue(withIdentifier:"unwindToCars", sender:self)
+           
     }
         
     }
@@ -158,32 +239,52 @@ class InspectionViewController : UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        rating1.setImage(UIImage(systemName:"1.circle"), for: .normal)
+        rating1.setImage(UIImage(systemName:"1.circle.fill"), for: .selected)
+        rating1.addTarget(self, action: #selector(getter: self.rating1), for: UIControl.Event.touchUpInside)
+        
+        
+        rating2.setImage(UIImage(systemName:"2.circle"), for: .normal)
+        rating2.setImage(UIImage(systemName:"2.circle.fill"), for: .selected)
+        rating2.addTarget(self, action: #selector(getter: self.rating2), for: UIControl.Event.touchUpInside)
+        
+        rating3.setImage(UIImage(systemName:"3.circle"), for: .normal)
+        rating3.setImage(UIImage(systemName:"3.circle.fill"), for: .selected)
+        rating3.addTarget(self, action: #selector(getter: self.rating3), for: UIControl.Event.touchUpInside)
+        
+        rating4.setImage(UIImage(systemName:"4.circle"), for: .normal)
+        rating4.setImage(UIImage(systemName:"4.circle.fill"), for: .selected)
+        rating4.addTarget(self, action: #selector(getter: self.rating4), for: UIControl.Event.touchUpInside)
+        
+        rating5.setImage(UIImage(systemName:"5.circle"), for: .normal)
+        rating5.setImage(UIImage(systemName:"5.circle.fill"), for: .selected)
+        rating5.addTarget(self, action: #selector(getter: self.rating5), for: UIControl.Event.touchUpInside)
         
         //Initialize the color buttons on the page. Check if one has already been selected
         //STILL NEED TO ADD DELEGATE METHOD
-        greenHexButton.setImage(UIImage(systemName:"hexagon"), for: .normal)
-        greenHexButton.setImage(UIImage(systemName:"hexagon.fill"), for: .selected)
-        greenHexButton.addTarget(self, action: #selector(getter: self.greenHexButton), for: UIControl.Event.touchUpInside)
-        if(checkList![page]! == 1){
-            greenHexButton.isSelected = true
-        }
-        
-        
-        yellowHexButton.setImage(UIImage(systemName:"hexagon"), for: .normal)
-        yellowHexButton.setImage(UIImage(systemName:"hexagon.fill"), for: .selected)
-        yellowHexButton.addTarget(self, action: #selector(getter: self.yellowHexButton), for: UIControl.Event.touchUpInside)
-        if(checkList![page]! == 2){
-            print("bitchass")
-            yellowHexButton.isSelected = true
-        }
-        
-        
-        redHexButton.setImage(UIImage(systemName:"hexagon"), for: .normal)
-        redHexButton.setImage(UIImage(systemName:"hexagon.fill"), for: .selected)
-        redHexButton.addTarget(self, action: #selector(getter: self.redHexButton), for: UIControl.Event.touchUpInside)
-        if(checkList![page]! == 3){
-            redHexButton.isSelected = true
-        }
+//        greenHexButton.setImage(UIImage(systemName:"hexagon"), for: .normal)
+//        greenHexButton.setImage(UIImage(systemName:"hexagon.fill"), for: .selected)
+//        greenHexButton.addTarget(self, action: #selector(getter: self.greenHexButton), for: UIControl.Event.touchUpInside)
+//        if(checkList![page]! == 1){
+//            greenHexButton.isSelected = true
+//        }
+//
+//
+//        yellowHexButton.setImage(UIImage(systemName:"hexagon"), for: .normal)
+//        yellowHexButton.setImage(UIImage(systemName:"hexagon.fill"), for: .selected)
+//        yellowHexButton.addTarget(self, action: #selector(getter: self.yellowHexButton), for: UIControl.Event.touchUpInside)
+//        if(checkList![page]! == 2){
+//            print("bitchass")
+//            yellowHexButton.isSelected = true
+//        }
+//
+//
+//        redHexButton.setImage(UIImage(systemName:"hexagon"), for: .normal)
+//        redHexButton.setImage(UIImage(systemName:"hexagon.fill"), for: .selected)
+//        redHexButton.addTarget(self, action: #selector(getter: self.redHexButton), for: UIControl.Event.touchUpInside)
+//        if(checkList![page]! == 3){
+//            redHexButton.isSelected = true
+//        }
         
         //continueButton.layer.cornerRadius = 4
         
--- AuctionReports/sendtoDB.swift
+++ AuctionReports/sendtoDB.swift
@@ -13,11 +13,11 @@ class sendtoDB:NSObject, URLSessionDelegate{
     
     func exportData(selectedCar: UncheckedCarsModel?, checkList: [Int:Int], commentsList: [Int:String]){
         
-        let url = NSURL(string:"mysql.wlgenerator.tech/export.php")
+        let url = NSURL(string:"https://www.wlgenerator.tech/export.php")
         var request = URLRequest(url: url! as URL)
         request.httpMethod = "POST"
         
-        for x in 1..<15{
+        for x in 1..<12{
             
             print("\(String(describing: checkList[x]!))" + "\(String(describing: commentsList[x]!))")
             
@@ -30,12 +30,10 @@ class sendtoDB:NSObject, URLSessionDelegate{
         dataString = dataString + "&year=\(selectedCar!.year!)"
         dataString = dataString + "&style=\(selectedCar!.style!)"
         dataString = dataString + "&color=\(selectedCar!.color!)"
-        dataString = dataString + "&CR=\(selectedCar!.CR!)"
         dataString = dataString + "&odometer=\(selectedCar!.odometer!)"
         dataString = dataString + "&grade=\(selectedCar!.grade!)"
-        dataString = dataString + "&lane=\(selectedCar!.lane!)"
-        dataString = dataString + "&lot=\(selectedCar!.lot!)"
         dataString = dataString + "&vin=\(selectedCar!.vin!)"
+        dataString = dataString + "&runNumber=\(selectedCar!.runNumber!)"
         dataString = dataString + "&baseMMR=\(selectedCar!.baseMMR!)"
         dataString = dataString + "&adjustedMMR=\(selectedCar!.adjustedMMR!)"
         dataString = dataString + "&lowRangeValue=\(selectedCar!.lowRangeValue!)"
@@ -43,67 +41,66 @@ class sendtoDB:NSObject, URLSessionDelegate{
         dataString = dataString + "&avgOdometer= \(selectedCar!.avgOdometer!)"
         dataString = dataString + "&avgCondition=\(selectedCar!.avgCondition!)"
         dataString = dataString + "&rank=\(selectedCar!.rank!)"
-        dataString = dataString + "&lights=\(String(describing:checkList[1]!) + commentsList[1]!)"
-        dataString = dataString + "&windshield=\(String(describing:checkList[2]!) +  commentsList[2]!)"
-        dataString = dataString + "&wipers=\(String(describing:checkList[3]!) + commentsList[3]!)"
-        dataString = dataString + "&horn=\(String(describing:checkList[4]!) +  commentsList[4]!)"
-        dataString = dataString + "&cabinfilter=\(String(describing:checkList[5]!) +  commentsList[5]!)"
-        dataString = dataString + "&A_C=\(String(describing:checkList[6]!) +  commentsList[6]!)"
-        dataString = dataString + "&airfilter=\(String(describing:checkList[7]!) +  commentsList[7]!)"
-        dataString = dataString + "&oil_fluids=\(String(describing:checkList[8]!) +  commentsList[8]!)"
-        dataString = dataString + "&belts=\(String(describing:checkList[9]!) +  commentsList[9]!)"
-        dataString = dataString + "&suspension=\(String(describing:checkList[10]!) +  commentsList[10]!)"
-        dataString = dataString + "&steering=\(String(describing:checkList[11]!) +  commentsList[11]!)"
-        dataString = dataString + "&exhaust=\(String(describing:checkList[12]!) +  commentsList[12]!)"
-        dataString = dataString + "&brakes=\(String(describing:checkList[13]!) +  commentsList[13]!)"
-        dataString = dataString + "&tires=\(String(describing:checkList[14]!) +  commentsList[14]!)"
-            
-   
+        
+        dataString = dataString + "&body=\(String(describing:checkList[1]!) + commentsList[1]!)"
+        dataString = dataString + "&tires=\(String(describing:checkList[2]!) +  commentsList[2]!)"
+        dataString = dataString + "&interior=\(String(describing:checkList[3]!) + commentsList[3]!)"
+        dataString = dataString + "&glass=\(String(describing:checkList[4]!) +  commentsList[4]!)"
+        dataString = dataString + "&battery=\(String(describing:checkList[5]!) +  commentsList[5]!)"
+        dataString = dataString + "&A_C_heat=\(String(describing:checkList[6]!) +  commentsList[6]!)"
+        dataString = dataString + "&engine=\(String(describing:checkList[7]!) +  commentsList[7]!)"
+        dataString = dataString + "&transmission=\(String(describing:checkList[8]!) +  commentsList[8]!)"
+        dataString = dataString + "&suspension=\(String(describing:checkList[9]!) +  commentsList[9]!)"
+        dataString = dataString + "&brakes=\(String(describing:checkList[10]!) +  commentsList[10]!)"
+        dataString = dataString + "&cooling=\(String(describing:checkList[11]!) +  commentsList[11]!)"
+       
         
         //Convert the string to uft8
         let convertedData = dataString.data(using: .utf8)!
         
-//        let uploadJob = URLSession.shared.uploadTask(with: request, from: convertedData){
-//
-//            data, response, error in
-//
-//            if error != nil {
-//
-//        // display an alert if there is an error inside the DispatchQueue.main.async
-//
-//            DispatchQueue.main.async
-//            {
-//                    print("connection failed")
-//            }
-//        }
-//else
-//{
-//    if let unwrappedData = data {
-//
-//        let returnedData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) // Response from web server hosting the database
-//
-//        if returnedData == "1" // insert into database worked
-//        {
-//
-//// display an alert if no error and database insert worked (return = 1) inside the DispatchQueue.main.async
-//            DispatchQueue.main.async
-//            {
-//                print("insert was successful")
-//            }
-//        }
-//        else
-//        {
-//// display an alert if an error and database insert didn't worked (return != 1) inside the DispatchQueue.main.async
-//            DispatchQueue.main.async
-//            {
-//
-//            print("database insert was not successful")
-//                }
-//            }
-//        }
-//    }
-//}
-                    //uploadJob.resume()
+        let uploadJob = URLSession.shared.uploadTask(with: request, from: convertedData){
+
+            data, response, error in
+
+            if error != nil {
+
+        // display an alert if there is an error inside the DispatchQueue.main.async
+
+            DispatchQueue.main.async
+            {
+                    print("connection failed")
+            }
+        }
+else
+{
+    if let unwrappedData = data {
+
+        let returnedData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) // Response from web server hosting the database
+
+        if returnedData == "1" // insert into database worked
+        {
+
+// display an alert if no error and database insert worked (return = 1) inside the DispatchQueue.main.async
+            DispatchQueue.main.async
+            {
+                print("insert was successful")
+            }
+        }
+        else
+        {
+// display an alert if an error and database insert didn't worked (return != 1) inside the DispatchQueue.main.async
+            DispatchQueue.main.async
+            {
+
+            print("database insert was not successful")
+            
+                
+                }
+            }
+        }
+    }
+}
+                    uploadJob.resume()
             
     }
     
