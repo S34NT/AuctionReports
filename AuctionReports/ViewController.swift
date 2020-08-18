535e2b2cca39f34274caf82d1e7781110c0a74ab
Sean <swg4941@wmich.edu>
2020-08-18 13:47:02 -0400

    Added more comments for functions in ViewController.swift

--- AuctionReports/ViewController.swift
+++ AuctionReports/ViewController.swift
@@ -37,14 +37,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
      
-
+        //Initialize the TableView
         self.ListTableView.delegate = self
         self.ListTableView.dataSource = self
         self.ListTableView.backgroundColor = .yellow
         self.ListTableView.reloadData()
 
     }
-    
     
     override func viewDidAppear(_ animated: Bool) {
         //Once the view appears, check if it is the first pass or not,
@@ -62,41 +61,49 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    //Remove anything that can be recreated
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     
+    //This function is called from HomeModel, and passes the
+    //data that is returned from the server
     func itemsDownloaded(items: NSMutableArray) {
-        
         feedItems = items
-        
     }
     
+    //Set the numbe of sections equal to the number of cars
+    //that were pulled from the server
     func numberOfSections(in tableView: UITableView) -> Int{
         return self.feedItems.count
     }
     
+    //set number of rows for a section to 1
     func tableView(_ tableView: UITableView,
     numberOfRowsInSection section: Int) -> Int{
         return 1
     }
     
+    
+    //set the Header color to clear
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?{
         let headerView = UIView()
         headerView.backgroundColor = UIColor.clear
         return headerView
     }
     
+    //This function defines the attributes of the reusable cells
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Retrieve cell
         let cellIdentifier: String = "basicCell"
         let myCell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
+        
         // Get the cars to be shown
         let item: UncheckedCarsModel = feedItems[indexPath.section] as! UncheckedCarsModel
-        // Get references to labels of cell
         
+        // Get references to labels of cell
         myCell.textLabel!.text = "\(item.year!) \(item.make!) \(item.model!)"
         
         myCell.textLabel!.textAlignment = .center
@@ -104,22 +111,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         myCell.layer.cornerRadius = 8
         myCell.layer.borderWidth = 1
         
-        
-        
         return myCell
     }
     
+    //Set the height of the header
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return cellSpacingHeight
     }
     
-    
+    //This function controls what happens when users interact with the UITableView
     func tableView(_ tableView: UITableView, didSelectRowAt IndexPath: IndexPath) {
 
-        
+        //When user selects a row, assign the value to selectedCar (UncheckedCarsModel Object)
         selectedRow = IndexPath
-        
-        
         selectedCar = feedItems[selectedRow.section] as! UncheckedCarsModel
       
         
@@ -135,22 +139,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if segue.destination is DetailViewController{
             
             
-            print("\(String(describing: selectedCar.model))") // Get reference to the destination view controller
+            // Get reference to the destination view controller
             let detailVC  = segue.destination as? DetailViewController
             
             // Set the property to the selected location so when the view for
-            // detail view controller loads, it can access that property to get the feeditem obj
+            // DetailViewController loads, it can access that property to get the feeditem obj
             detailVC?.selectedCar = selectedCar
             detailVC?.selectedRow = selectedRow
-        
-
-    }
+        }
 
     }
     
     //unwinder function that is called by the unwindToCars Segue
+    //(Called from the InspectionViewController)
     @IBAction func unwinder( _ seg: UIStoryboardSegue) {
         
+        //toggle firstpass so the car will be removed when ViewDidAppear is called
         self.firstPass = false
         
         //First remove all viewControllers that were created from the last car inspection
